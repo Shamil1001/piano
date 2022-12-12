ae96165b849685d393cc8480e4181d68a2609fd9
Shamil <shjemhurov@gmail.com>
2022-12-12 13:22:19 +0500

    background color changed

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function Buttons() {
   const pianoKeys = document.querySelectorAll(".key");
 
   pianoKeys.forEach((pianoKey, i) => {
-    const num = i < 9 ? "0" + i : i + 1;
+    const num = i < 9 ? "0" + (i + 1) : i + 1;
     const url = "./24pianoKeys/key" + num + ".mp3";
+    console.log(url);
     pianoKey.addEventListener("click", () => {
       new Audio(url).play();
     });
