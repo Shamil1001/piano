ef420f686798fe732e63fbbe457b3530b0dd5b67
Shamil <shjemhurov@gmail.com>
2022-12-14 19:34:46 +0500

    console logs and comments are deleted

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function Buttons() {
   pianoKeys.forEach((pianoKey, i) => {
     const num = i < 9 ? "0" + (i + 1) : i + 1;
     const url = "./24pianoKeys/key" + num + ".mp3";
-    console.log(url);
     pianoKey.addEventListener("click", () => {
       new Audio(url).play();
     });
