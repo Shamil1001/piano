377c21a41ee40693b466ca725eed6b75906b2a4e
Shamil <shjemhurov@gmail.com>
2022-12-19 15:02:50 +0500

    audios added blacks and whites separately

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function Buttons() {
-  const pianoKeys = document.querySelectorAll(".key");
+  const blacks = document.querySelectorAll(".black");
+  const whites = document.querySelectorAll(".white");
 
-  pianoKeys.forEach((pianoKey, i) => {
-    const num = i < 9 ? "0" + (i + 1) : i + 1;
-    const url = "./24pianoKeys/key" + num + ".mp3";
-    pianoKey.addEventListener("click", () => {
-      new Audio(url).play();
+  blacks.forEach((black, i) => {
+    const n = i + 15;
+    const urlb = "./24pianoKeys/key" + n + ".mp3";
+    black.addEventListener("click", () => {
+      new Audio(urlb).play();
+    });
+  });
+
+  whites.forEach((white, j) => {
+    const n = j < 9 ? "0" + (j + 1) : j + 1;
+    console.log(n);
+    const urlw = "./24pianoKeys/key" + n + ".mp3";
+    white.addEventListener("click", () => {
+      new Audio(urlw).play();
     });
   });
 }
